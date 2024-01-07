75e8a53eb266c1d356b5e0f5261a06281fd47184
hiro <vuonghoainam.work@gmail.com>
2024-01-07 10:05:07 +0700

    feat: Add n-gpu and n-vram to system bottom bar

--- web/containers/Layout/BottomBar/index.tsx
+++ web/containers/Layout/BottomBar/index.tsx
@@ -43,7 +43,7 @@ const menuLinks = [
 
 const BottomBar = () => {
   const { activeModel, stateModel } = useActiveModel()
-  const { ram, cpu } = useGetSystemResources()
+  const { ram, cpu, nvidiaGpuUtilization, nvidiaVramUtilization } = useGetSystemResources()
   const progress = useAtomValue(appDownloadProgress)
   const { downloadedModels } = useGetDownloadedModels()
   const { setMainViewState } = useMainViewState()
@@ -95,6 +95,8 @@ const BottomBar = () => {
         <div className="flex items-center gap-x-2">
           <SystemItem name="CPU:" value={`${cpu}%`} />
           <SystemItem name="Mem:" value={`${ram}%`} />
+          <SystemItem name="N-GPU:" value={`${nvidiaGpuUtilization}%`} />
+          <SystemItem name="N-VRAM:" value={`${nvidiaVramUtilization}%`} />
         </div>
         {/* VERSION is defined by webpack, please see next.config.js */}
         <span className="text-xs">Jan v{VERSION ?? ''}</span>
